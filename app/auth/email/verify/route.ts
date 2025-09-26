52c69ac86fdeb66d18e2bfb23c1328a440e935d7
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-09-26 23:07:11 +0700

    fix: add missing origin to url construction
    Also inialize new constant for origin and supabase url to promode DRY mindset with shorter variable name

--- app/auth/email/verify/route.ts
+++ app/auth/email/verify/route.ts
@@ -4,41 +4,43 @@ export async function GET(req: NextRequest) {
   // Extract Parameters
   const url = req.nextUrl;
   const { token, type, email } = Object.fromEntries(url.searchParams.entries());
+  const origin = process.env.NEXT_PUBLIC_APP_URL;
+  const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
   // Validate required environment variables
-  if (!process.env.NEXT_PUBLIC_APP_URL) {
+  if (!origin) {
     return NextResponse.redirect(
       "/auth?code=server_error&message=" +
         encodeURIComponent("Missing app URL configuration")
     );
   }
 
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  if (!supabaseURL) {
     return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_APP_URL}/auth?code=server_error&message=${encodeURIComponent("Missing Supabase URL configuration")}`
+      `${origin}/auth?code=server_error&message=${encodeURIComponent("Missing Supabase URL configuration")}`
     );
   }
 
   if (!token) {
     return NextResponse.redirect(
-      `/auth?code=bad_request&message=${encodeURIComponent("Missing token parameter")}`
+      `${origin}/auth?code=bad_request&message=${encodeURIComponent("Missing token parameter")}`
     );
   }
 
   if (!type) {
     return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_APP_URL}/auth?code=bad_request&message=${encodeURIComponent("Missing type parameter")}`
+      `${origin}/auth?code=bad_request&message=${encodeURIComponent("Missing type parameter")}`
     );
   }
 
   if (!email) {
     return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_APP_URL}/auth?code=bad_request&message=${encodeURIComponent("Missing email parameter")}`
+      `${origin}/auth?code=bad_request&message=${encodeURIComponent("Missing email parameter")}`
     );
   }
 
   // Construct URL
-  const confirmedURL = `${process.env.NEXT_PUBLIC_APP_URL}/auth/email/confirmed?email=${email}`;
+  const confirmedURL = `${origin}/auth/email/confirmed?email=${email}`;
   const verifyURL = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/verify?token=${token}&type=${type}&redirect_to=${confirmedURL}`;
 
   return NextResponse.redirect(verifyURL);
