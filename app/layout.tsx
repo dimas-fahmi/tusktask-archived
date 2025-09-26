fff6a0aa1473b0f06c0cb9d6c6a20a602db962cd
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-09-26 23:10:43 +0700

    fix: initialize query client inside component = issue
    Moved queryClient initialization outside component, this client should be created once and used multiple time

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,14 @@ import { spaceGroteskFont, oswaldFont } from "@/src/ui/fonts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+// QueryClient Initialization
+const queryClient = new QueryClient();
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // QueryClient Initialization
-  const queryClient = new QueryClient();
-
   return (
     <html lang="en">
       <body
