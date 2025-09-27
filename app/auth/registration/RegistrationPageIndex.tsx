ca5d25be93edb1d886808a6c961a3c17dd83b922
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-09-27 10:06:57 +0700

    feat: add new page for onboarding process
    Initialize two file server component and client index for registration/onboarding page

---
+++ app/auth/registration/RegistrationPageIndex.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import NavLink from "@/src/ui/components/NavLink";
+import AuthAlert from "@/src/ui/components/Prefabs/AuthAlert";
+import React, { Suspense } from "react";
+
+const RegistrationPageIndex = () => {
+  return (
+    <div>
+      {/* Header */}
+      <header className="mt-4">
+        <h1 className="font-header font-bold text-2xl mb-2">
+          Email Confirmation
+        </h1>
+        <p className="text-sm">
+          {`This action is intended to prevent you from being locked out of your account.`}
+        </p>
+      </header>
+
+      {/* Auth Alert */}
+      <Suspense>
+        <AuthAlert className="mt-6" />
+      </Suspense>
+
+      <form className="mt-6 space-y-4">
+        {/* Email */}
+        {/* <Input
+          name="email"
+          aria-placeholder="Email"
+          placeholder="Email"
+        /> */}
+      </form>
+
+      {/* Helper Bar */}
+      <div className="mt-4 text-sm font-light flex items-center justify-between">
+        <span>{`It's not your account?`}</span>
+        <NavLink href="/auth">Sign Out</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationPageIndex;
--- app/auth/registration/page.tsx
+++ app/auth/registration/page.tsx
@@ -1,13 +1,40 @@
+import { VERCEL_BLOB_HOST } from "@/src/lib/configs";
 import { generateMetadata } from "@/src/lib/utils/generateMetadata";
+import NavLink from "@/src/ui/components/NavLink";
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import RegistrationPageIndex from "./RegistrationPageIndex";
 
 export const metadata: Metadata = generateMetadata({
   title: "Registration Phase",
 });
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  return (
+    <div className="max-w-md p-4">
+      {/* Logo */}
+      <Link href={"/"}>
+        <Image
+          width={250}
+          height={250}
+          src={`${VERCEL_BLOB_HOST}/logo/tusktask.png`}
+          alt="TuskTask Logo Symbolic"
+          className="w-16 h-16 block"
+        />
+      </Link>
+
+      {/* Registration */}
+      <RegistrationPageIndex />
+
+      {/* Policy */}
+      <footer className="mt-6 text-xs">
+        By this you agree to our{" "}
+        <NavLink href="/policy">privacy policy</NavLink>
+      </footer>
+    </div>
+  );
 };
 
 export default RegistrationPage;
