b6896674a007d690b720785a10f7b6a4a3830cec
dimas-fahmi <dimasfahmipebrian@gmail.com>
2025-10-12 11:20:51 +0700

    fix: padding for mobile

--- src/ui/components/Dashboard/TaskAccordion/index.tsx
+++ src/ui/components/Dashboard/TaskAccordion/index.tsx
@@ -122,7 +122,7 @@ const body = ({ children }: { children: React.ReactNode }) => {
       }
       className="overflow-hidden"
     >
-      <div className={`grid grid-cols-1 md:grid-cols-3 gap-4 p-4`}>
+      <div className={`grid grid-cols-1 md:grid-cols-3 gap-4 md:p-4`}>
         {children}
       </div>
     </motion.div>
